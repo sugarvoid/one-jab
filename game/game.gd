0500921e359fc1ecb4c0f3dd48827d11d92bea63
Dave <gamedev@kitargo.com>
2024-02-23 19:32:01 -0600

    Clean up code

--- game/game.gd
+++ game/game.gd
@@ -3,18 +3,10 @@ extends Node2D
 @onready var player: AnimatedSprite2D = get_node("PlayScreen/Player")
 @onready var punching_bag: AnimatedSprite2D = get_node("PlayScreen/PunchingBag")
 @onready var marker: AnimatedSprite2D = get_node("PlayScreen/marker")
+@onready var lbl_hits_left: Label = get_node("PlayScreen/lblHits")
+@onready var lbl_time: Label = get_node("PlayScreen/LblTime")
 
-const HITS_NEEDED: int = 50; 
-
-var game_state: int
-var hits_left: int
-var bag_slot: int
-var player_slot: int
-var lives: int
-
-
-var bags_next_pos: int
-
+const HITS_NEEDED: int = 50
 const slots: Array[int] = [
 	14,
 	34,
@@ -25,14 +17,22 @@ const slots: Array[int] = [
 	134
 ]
 
+var game_state: int
+var hits_left: int
+var bag_slot: int
+var player_slot: int
+var lives: int
+var bags_next_pos: int
+
+
 func _ready():
 	switch_screen(0)
 	update_hud()
 	marker.play("default")
 
 
-func _process(delta):
-	$PlayScreen/LblTime.text = $PlayScreen/Stopwatch.get_time()
+func _process(_delta: float):
+	lbl_time.text = $PlayScreen/Stopwatch.get_time()
 	update_player()
 	update_bag()
 
@@ -40,25 +40,27 @@ func _process(delta):
 func update_bag():
 	marker.position = Vector2(slots[bags_next_pos], 85)
 	if bag_slot < player_slot:
-		$PlayScreen/PunchingBag.scale.x = 1
+		punching_bag.scale.x = 1
 	elif bag_slot > player_slot:
-		$PlayScreen/PunchingBag.scale.x = -1
-	$PlayScreen/PunchingBag.position = Vector2(slots[bag_slot], 85)
+		punching_bag.scale.x = -1
+	punching_bag.position = Vector2(slots[bag_slot], 85)
 
 
 func update_player():
 	if bag_slot < player_slot:
-		$PlayScreen/Player.scale.x = 1
+		player.scale.x = 1
 	elif bag_slot > player_slot:
-		$PlayScreen/Player.scale.x = -1
-	$PlayScreen/Player.position = Vector2(slots[player_slot], 88)
+		player.scale.x = -1
+	player.position = Vector2(slots[player_slot], 88)
+
 
 func update_hud():
-	$PlayScreen/lblHits.text = str(hits_left)
+	lbl_hits_left.text = str(hits_left)
 	
 
 func switch_screen(new_screen: int):
 	self.game_state = new_screen
+	$PlayScreen/Stopwatch.stop()
 	match new_screen:
 		0:
 			$PlayScreen.hide()
@@ -75,7 +77,6 @@ func switch_screen(new_screen: int):
 			$PlayScreen.show()
 			$StartScreen.hide()
 			$GameOver.hide()
-			$PlayScreen/TimeLeft.start();
 		2:
 			$PlayScreen.hide()
 			$StartScreen.hide()
@@ -84,8 +85,8 @@ func switch_screen(new_screen: int):
 
 func player_punch():
 	if player_slot == (bag_slot - 1) or player_slot == bag_slot + 1:
-		$PlayScreen/Player.play("punch")
-		$PlayScreen/PunchingBag.play("hit")
+		player.play("punch")
+		punching_bag.play("hit")
 		hits_left -= 1
 		update_hud()
 		move_bag()
@@ -103,15 +104,13 @@ func move_bag():
 	bags_next_pos = new_pos
 
 
-func get_new_pos(slot: int) -> Vector2:
-	return Vector2(100,100)
-
-
 func _input(event):
 	match game_state:
 		0:
 			if event.is_action_pressed("move") or event.is_action_pressed("poke"):
 				self.switch_screen(1)
+			if event.is_action_pressed("ui_cancel"):
+				get_tree().quit()
 		1:
 			if event.is_action_pressed("poke"):
 				player_punch()
--- game/stopwatch.gd
+++ game/stopwatch.gd
@@ -1,17 +1,16 @@
 extends Node
 
-var running = false
-var start_time = 0
-
+var running: bool = false
+var start_time: int = 0
 
 
 func _ready():
 	start()
-	
-func _process(delta):
+
+func _process(_delta: float):
 	if running:
 		get_time()
-		
+
 
 func start():
 	running = true
@@ -19,26 +18,15 @@ func start():
 	
 
 func get_time():
-	var elapsed = (Time.get_ticks_msec() - start_time) #/ 1000.0
-	##var elapsed = time_now - start_time
+	var elapsed: int = (Time.get_ticks_msec() - start_time)
 	
-	var minutes = int(elapsed / 60 / 1000)
+	var minutes = elapsed / 60 / 1000
 	var seconds = elapsed / 1000 % 60
-	var miliseconds: int = elapsed / 10 % 100
+	var miliseconds = elapsed / 10 % 100
 	
-	#var elapsed_time = "%02d:%02d:%02d" % [hours ,minutes, seconds]
-	#var time = ("%02d" % minutes) + (":%02d" % seconds) + (":%02d" % miliseconds)
 	var labelText = "%02d:%02d:%02d" % [minutes, seconds, miliseconds]
 	return labelText
 	
 func stop():
 	running = false
-
-func _on_Button_pressed():
-	if !running:
-		start()	
-	else:
-		stop()
-
-
 
